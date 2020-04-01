10d4c29503843000c71193b61c28fd0f63c86acd
Xotonori <xotonori29011991@gmail.com>
2020-04-01 22:03:17 +0300

    CodewarsTask01 updated file

--- src/components/CodewarsTask01.js
+++ src/components/CodewarsTask01.js
@@ -8,31 +8,37 @@ import React from 'react';
 // Note: If the number is a multiple of both 3 and 5, only count it once.
 
 const CodewarsTask01 = () => {
-        function solution(number) {
-            const acceptedNumber = number;
-            const multipleNumbers = [3, 5];
-            const checkedNaturalNumbers = [];
 
-            for (let i = 0; i < acceptedNumber; i++) {
-                for (let j = 0; j < multipleNumbers.length; j++) {
-                    if (i % multipleNumbers[j] === 0) {
-                        if (checkedNaturalNumbers.indexOf(i) === -1) {
-                            checkedNaturalNumbers.push(i)
-                        }
+    function solution(number) {
+
+        if (number === 0 || typeof number !== 'number') {
+            return 0;
+        }
+
+        const acceptedNumber = Math.abs(number);
+        const multipleNumbers = [3, 5];
+        const checkedNaturalNumbers = [];
+
+        for (let i = 0; i < acceptedNumber; i++) {
+            for (let j = 0; j < multipleNumbers.length; j++) {
+                if (i % multipleNumbers[j] === 0) {
+                    if (checkedNaturalNumbers.indexOf(i) === -1) {
+                        checkedNaturalNumbers.push(i)
                     }
                 }
             }
-
-            const reducer = (accumulator, currentValue) => accumulator + currentValue;
-            return checkedNaturalNumbers.reduce(reducer);
         }
 
-        return (
-            <div>
-                Результат: {solution(10)}
-                <hr/>
-            </div>
-        )
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return checkedNaturalNumbers.reduce(reducer);
+    }
+
+    return (
+        <div>
+            Результат: {solution(10)}
+            <hr/>
+        </div>
+    )
 };
 
 export default CodewarsTask01;
